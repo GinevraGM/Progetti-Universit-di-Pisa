2c380a7bddeabfeeacd943cf646befc2c5db1771
GinevraGM <47157564+GinevraGM@users.noreply.github.com>
2024-06-15 16:25:46 +0200

    Delete GinevraMaoro-581700 directory

--- GinevraMaoro-581700/includes/Collector.h
+++
@@ -1,6 +0,0 @@
-#ifndef COLLECTOR_H
-#define COLLECTOR_H
-
-int Collector(char * socket_name);
-
-#endif
--- GinevraMaoro-581700/includes/Worker_consumer.h
+++
@@ -1,7 +0,0 @@
-#ifndef WORKER_CONSUMER_H
-#define WORKER_CONSUMER_H
-
-
-void *Worker_consumer(void *arg);
-
-#endif
